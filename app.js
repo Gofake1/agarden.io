f176aa2783877a52510704d5c6a903c007ce78fc
tderanek117 <tderanek@nd.edu>
2015-09-29 19:36:40 -0400

    Player window sizes to browser window according to its width

--- app.js
+++ app.js
@@ -51,8 +51,12 @@ ctx.canvas.height = window.innerHeight;
 // Total number of tiles in game 
 var gridHeight = 50;
 var gridWidth = 100;
-var tileLength_start = 50;
-var tileLength_min = 25;
+var numTiles_x_start = 20;
+var numTiles_x_max = 30;
+// number of visible tiles (width)
+var numTiles_x = numTiles_x_start;
+var tileLength = window.innerWidth / numTiles_x;
+
 
 //  Global variables
 var board = Board(gridHeight, gridWidth, 0);
@@ -78,6 +82,7 @@ var players = [
 
 document.addEventListener('mousemove', mouseInput, false);
 
+// This may be obsolete with the introduction of drawSprite_exact()
 // Draws a sprite at a specified location
 function drawSprite(xpos, ypos, src, scalar, offset, alpha) {
     var sprite = new Image();
@@ -92,6 +97,20 @@ function drawSprite(xpos, ypos, src, scalar, offset, alpha) {
         ctx.scale(1/scalar, 1/scalar);
         ctx.globalAlpha = 1;
     };
+}
+
+// This function will draw an image in the exact dimensions we want.
+// It will be useful for resizing tiles as the window resizes
+function drawSprite_exact(src, x, y, w, h, alpha)
+{
+    var sprite = new Image();
+    sprite.src = src;
+    sprite.onload = function()
+    ctx.globalAlpha = alpha;
+    {
+        ctx.drawImage(sprite, x, y, w, h);
+    }
+    ctx.globalAlpha = 1;
 }
 
 // MAKE SURE TO SEPARATE STUFF OUT LATER!!!!!
@@ -123,27 +142,20 @@ function drawGrid(xmin, ymin, xmax, ymax, tileLength)
                 ctx.strokeRect(x*tileLength-xmin, y*tileLength-ymin, tileLength, tileLength);
                 switch (board[y][x]) 
                 {
-                case (0):
-                    drawSprite(x*tileLength-xmin, y*tileLength-ymin, 'sprites/dirt.jpg', .191, 0);
+                case (0): // DIRT
+                    drawSprite_exact('sprites/dirt.jpg', x*tileLength-xmin, y*tileLength-ymin, tileLength, tileLength, 1);
                     break;
-                case (1):
+                case (1): // RED PLANT
                     ctx.fillStyle = 'red';
                     ctx.fillRect(x*tileLength-xmin, y*tileLength-ymin, tileLength-1, tileLength-1);
-                    drawSprite(x*tileLength-xmin, y*tileLength-ymin, 'sprites/plant.png', .245, 0, .6);
+                    drawSprite_exact('sprites/plant.png', x*tileLength-xmin, y*tileLength-ymin, tileLength, tileLength, .6);
                     break;
-                case (2):
+                case (2): // PURPLE PLANT
                     ctx.fillStyle = 'purple';
                     ctx.fillRect(x*tileLength-xmin, y*tileLength-ymin, tileLength-1, tileLength-1);
-                    drawSprite(x*tileLength-xmin, y*tileLength-ymin, 'sprites/plant.png', .245, 0, .6);
-                    break;                
-                    ctx.fillRect(x*tileLength, y*tileLength, tileLength, tileLength);
-                    drawSprite(x, y, 'sprites/plant.png', .07, 0, .7);                    break;
-                case (2):
-                    ctx.fillStyle = 'purple';
-                    ctx.fillRect(x*tileLength, y*tileLength, tileLength, tileLength);
-                    drawSprite(x, y, 'sprites/plant.png', .07, 0, .7);
+                    drawSprite_exact('sprites/plant.png', x*tileLength-xmin, y*tileLength-ymin, tileLength, tileLength, .6);
                     break;
-                default:
+                default: // ERROR
                     ctx.fillStyle = 'black';
                     ctx.fillRect(x*tileLength-xmin, y*tileLength-ymin, tileLength, tileLength);
                     break;
@@ -155,6 +167,8 @@ function drawGrid(xmin, ymin, xmax, ymax, tileLength)
 
 function drawOverlayer(xmin, ymin, xmax, ymax, tileLength)
 {
+    // Temp variable to curb ridiculously long lines
+    var tl = tileLength;
     // 'w' could stand for water, currently just testing to see if it works
     overlayer[3][28] = 1;
     overlayer[4][29] = 1;
@@ -165,18 +179,18 @@ function drawOverlayer(xmin, ymin, xmax, ymax, tileLength)
     {
         for (var x = 0; x < gridWidth; x++) 
         {
-        	if (x*tileLength>=xmin-tileLength && x*tileLength<xmax && y*tileLength>=ymin-tileLength && y*tileLength<ymax)
+        	if (x*tileLength>=xmin-tl && x*tl<xmax && y*tl>=ymin-tl && y*tl<ymax)
             {
 	            switch (overlayer[y][x]) 
 	            {
 	                case (0):
 	                    break;
 	                case (1):
-	                    drawSprite(x*tileLength, y*tileLength, 'sprites/water_bucket.png', .25, 22);
+                        drawSprite_exact('sprites/water_bucket.png', x*tl-xmin+tl/9, y*tl-ymin+tl/9, tl*3/4, tl*3/4, 1);
 	                    break;
 	                case ('H'):
 						// This is the case where the tile is a house space
-						drawSprite(x*tileLength-xmin, y*tileLength-ymin, 'sprites/house.png', .245, 15);
+						drawSprite_exact( 'sprites/house.png',x*tl-xmin, y*tl-ymin, tl, tl, 1);
 						break;
 	            }
 	        }
@@ -243,9 +257,9 @@ function drawPlayers() {
 }
 
 // TEST, CUT LATER
-function drawPlayer() {
+function drawPlayer(xmin, ymin) {
 	ctx.beginPath();
-	ctx.arc(thisPlayer.x, thisPlayer.y, 25, 0, 2*Math.PI, false);
+	ctx.arc(thisPlayer.x-xmin, thisPlayer.y-ymin, tileLength/2.25, 0, 2*Math.PI, false);
 	ctx.fillStyle = thisPlayer.color;
 	ctx.fill();
 }
@@ -262,7 +276,7 @@ function mouseInput(mouse) {
     }
 
     if (distX !== 0 && distY !== 0) {
-        angle = Math.atan2(distx, distY*-1);
+        angle = Math.atan2(distX, distY*-1);
         player.x -= (player.x - (mouse.clientX/player.speed));
         player.y -= (player.y - (mouse.clientY/player.speed));
     }
@@ -276,17 +290,25 @@ function keyInput(){
 // This function will dynamically follow the player, drawing the board
 // around him/her as he moves in realtime
 function drawPlayerWindow()  {
-    // Record the board's visible bounds
-    var min_x = thisPlayer.x<tileLength_start*10 ? 0 : thisPlayer.x-tileLength_start*10;
-    var min_y = thisPlayer.y<tileLength_start*10 ? 0 : thisPlayer.y-tileLength_start*10;
-    var max_x = min_x+tileLength_start*30;
-    var max_y = min_y+tileLength_start*30;
+    var numTiles_y = window.innerHeight / tileLength + 1;
+    var halfX = numTiles_x / 2;
+    var halfY = numTiles_y / 2;
 
-    drawGrid(min_x, min_y, max_x, max_y, tileLength_start);
-    drawOverlayer(min_x, min_y, max_x, max_y, tileLength_start);
+    // Record the board's visible bounds
+    var min_x = thisPlayer.x<tileLength*halfX ? 0 : thisPlayer.x-tileLength*halfX;
+    var min_y = thisPlayer.y<tileLength*halfY ? 0 : thisPlayer.y-tileLength*halfY;
+    var max_x = min_x+tileLength*numTiles_x;
+    var max_y = min_y+tileLength*numTiles_y;
+
+    // Draw that board!
+    drawGrid(min_x, min_y, max_x, max_y, tileLength);
+    drawOverlayer(min_x, min_y, max_x, max_y, tileLength);
+
+    // And the player too!
+    drawPlayer(min_x, min_y);
 }
 
-// This function is used to load images prior to their use
+// This function will be used to load images prior to their use
 // so that there is no draw delay
 // Draw delay obscures the leaderboard and other text overlay
 function init_images() {
@@ -301,7 +323,7 @@ function setDelta() {
     // delta = (now-then)/1000;
 
     now = (new Date()).getTime();
-    delta = (now-then)/1000;
+    delta = (now-then)/1000;    // WHAT IS THEN?
     then = now;
 }
 
@@ -323,12 +345,10 @@ var lastTime;
 function gameLoop()
 {
     drawPlayerWindow();
-    drawOverlayer(tileLength_start);
     // drawPlayers(); // obsolete function call
     drawLeaderboard();
     drawScore();
     drawCurrentPowerup();
-    drawPlayer();
     var now = Date.now();
     var dt = (now - lastTime) / 1000.0;
     setDelta();
@@ -352,6 +372,7 @@ function init() {
 }
 
 gameLoop();
+
 
 
 
