d7ffdb57c730f5a03ebba107afe49f0b672f281a
David Wu <david@gofake1.net>
2015-10-06 22:11:10 -0400

    Keyboard works now

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var Board = function(numRows, numCols, value) {
     return this.array;
 };
 
-var thisPlayer = {x:0, y:0, speed:25, color:'blue', score:0};
+var thisPlayer = {x:0, y:0, speed:5, color:'blue', score:0};
 var otherPlayers = [];
 
 var canvas = document.getElementById('canvas');
@@ -253,9 +253,8 @@ function drawPlayer(xmin, ymin) {
 function keyInput(key) {
     // Use powerup
     if (key.keyCode == 32) {
-
+        alert('Powerup used!');
     }
-    alert(key.keyCode);
 }
 
 // Handles mouse movement
