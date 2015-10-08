7f6ff0db4316f4d8af3c0d1ef176453684a47b8f
tderanek117 <tderanek@nd.edu>
2015-10-08 15:48:16 -0400

    Picking up powerups works! =D
    Also:
     - keypresses do not work at all in Firefox,
    they give an odd console error that I don't
    full understand

--- app.js
+++ app.js
@@ -46,7 +46,28 @@ var vizmax_y = vizmin_y+board_tileLength*numTiles_y;
 
 // Game variables
 var board = Board(gridHeight, gridWidth, 0);
+    // Red Farmer
+    board[3][8] = 1;
+    board[3][9] = 1;
+    board[4][8] = 1;
+    board[5][8] = 1;
+    board[4][9] = 1;
+    board[5][9] = 1;
+    board[3][10] = 1;
+    // Purple Farmer
+    board[35][70] = 2;
+    board[36][70] = 2;
+    board[37][70] = 2;
+    board[36][71] = 2;
+    board[36][72] = 2;
+    board[35][71] = 2;
+
 var overlayer = Board(gridHeight, gridWidth, 0);
+    overlayer[3][28] = 1;
+    overlayer[4][29] = 1;
+    overlayer[7][7] = 1;
+    overlayer[4][8] = 2;
+
 var leaderboard = [];
 
 // Sprites
@@ -94,7 +115,7 @@ function updateBoardVars() {
 // Gets the player's entered name
 function getName() {
             var pname = document.getElementById("pname").value;
-            console.log(pname);
+            //console.log(pname);
             leaderboard.push(pname);
 }
 
@@ -108,23 +129,6 @@ function drawSprite(img, x, y, w, h, alpha) {
 
 // MAKE SURE TO SEPARATE STUFF OUT LATER!!!!!
 function drawGrid(xmin, ymin, xmax, ymax, board_tileLength) {
-    // Red Farmer
-    board[3][8] = 1;
-    board[3][9] = 1;
-    board[4][8] = 1;
-    board[5][8] = 1;
-    board[4][9] = 1;
-    board[5][9] = 1;
-    board[3][10] = 1;
-
-    // Purple Farmer
-    board[35][70] = 2;
-    board[36][70] = 2;
-    board[37][70] = 2;
-    board[36][71] = 2;
-    board[36][72] = 2;
-    board[35][71] = 2;
-
     for (var y = 0; y < gridHeight; y++) {
         for (var x = 0; x < gridWidth; x++) {
             xLength = x*board_tileLength;
@@ -158,11 +162,6 @@ function drawGrid(xmin, ymin, xmax, ymax, board_tileLength) {
 function drawOverlayer(xmin, ymin, xmax, ymax, board_tileLength) {
     // Temp variable to curb ridiculously long lines
     var tl = board_tileLength;
-    // 'w' could stand for water, currently just testing to see if it works
-    overlayer[3][28] = 1;
-    overlayer[4][29] = 1;
-    overlayer[7][7] = 1;
-    overlayer[4][8] = 2;
 
     for (var y = 0; y < gridHeight; y++) {
         for (var x = 0; x < gridWidth; x++) {
@@ -221,7 +220,7 @@ function drawCurrentPowerup() {
     ctx.globalAlpha = 0.4;
     ctx.strokeRect(10, 10, 250, 50);
     ctx.fillStyle = 'black';
-    ctx.fillRect(10, 10, 250, 50);
+    ctx.fillRect(10, 10, 300, 50);
 
     ctx.globalAlpha = 0.9;
     ctx.fillStyle = 'white';
@@ -239,6 +238,9 @@ function drawCurrentPowerup() {
     case(1):
     	// water bucket
     	powstring += "Water Bucket";
+    	break;
+    default:
+    	powstring += "ERROR!!!";
     	break;
     }
     ctx.fillText(powstring, 15, 40);
@@ -277,6 +279,7 @@ function keyInput(key) {
     // Use powerup
     if (key.keyCode == 32) {
         alert('Powerup used!');
+        thisPlayer.powerup = 0;
     }
 }
 
@@ -364,7 +367,7 @@ function processOverlayer()
 	yTile = Math.floor(thisPlayer.y / objective_tileLength);
 
 	// The object of the tile you are currently standing on
-	var current = overlayer[yTile][xTile];
+	current = overlayer[yTile][xTile];
 	switch (current)
 	{
 	case(0):
@@ -374,10 +377,12 @@ function processOverlayer()
 		// water bucket
 		thisPlayer.powerup = 1;
 		break;
+	default:
+		break;
 	}
-	thisPlayer.powerup = current;
 
 	overlayer[yTile][xTile] = 0;
+	console.log("Current Tile Contents: " + overlayer[yTile][xTile]);
 }
 
 
